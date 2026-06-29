ee4d206e708311ef639b44bdc8cdf2090650a4ad
{Harshit <{Harshitkumar135267@gmail.com}>
2026-06-29 23:12:39 +0530

    Add search input validation

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,12 @@ def home():
     return{
         "message":"Welcome to my AI Search Engine!"
     }
+@app.get("/search")
+def search(query:str):
+    if query=="":
+        return{
+            "error":"Please enter a search term"
+        }
+    return{
+        "search":query
+    }
